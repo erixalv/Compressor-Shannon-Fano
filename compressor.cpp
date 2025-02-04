37697e7982b4191c05b38ebefdd2c8cb205d9a9c
Eric Alves <eralolca@gmail.com>
2025-02-03 23:00:35 -0300

    fixing errors

--- compressor.cpp
+++ compressor.cpp
@@ -8,17 +8,20 @@
 
 using namespace std;
 
-// Função para codificar o texto
-vector<uint8_t> codificarTexto(const string& texto, const unordered_map<char, string>& mapaCodigo, int& totalBits, int& numCaracteresCodificados) {
+// Função para codificar o texto e exibir os bits no terminal
+vector<uint8_t> codificarTexto(const string& texto, const unordered_map<char, string>& mapaCodigo, int& totalBits, int& numCaracteresCodificados, string& bitsCodificados) {
     vector<uint8_t> textoCodificado;
     string buffer;
     totalBits = 0;
     numCaracteresCodificados = 0;
+    bitsCodificados = "";
 
     for (char c : texto) {
         if (mapaCodigo.find(c) != mapaCodigo.end()) {
-            buffer += mapaCodigo.at(c);
-            totalBits += mapaCodigo.at(c).size();
+            string codigo = mapaCodigo.at(c);
+            bitsCodificados += codigo;  // Acumula a sequência de bits
+            buffer += codigo;
+            totalBits += codigo.size();
             numCaracteresCodificados++;
 
             while (buffer.size() >= 8) {
@@ -43,23 +46,24 @@ vector<uint8_t> codificarTexto(const string& texto, const unordered_map<char, st
 
 int main() {
     unordered_map<char, string> mapaCodigo = {
-        {'a', "010"}, {'b', "1111101"}, {'c', "11100"},
-        {'d', "10111"}, {'e', "001"}, {'f', "1111110"},
-        {'g', "1111010"}, {'h', "1111011"}, {'i', "1010"},
-        {'j', "111111110"}, {'k', "11111111110"}, {'l', "111010"},
-        {'m', "1100"}, {'n', "10110"}, {'o', "011"},
-        {'p', "111011"}, {'q', "1111100"}, {'r', "1001"},
-        {'s', "1000"}, {'t', "11011"}, {'u', "11010"},
-        {'v', "1111000"}, {'w', "111111111110"}, {'y', "111111111111"}, {'z', "11111110"}, {' ', "000"},
-        {'A', "010"}, {'B', "1111101"}, {'C', "11100"},
-        {'D', "10111"}, {'E', "001"}, {'F', "1111110"},
-        {'G', "1111010"}, {'H', "1111011"}, {'I', "1010"},
-        {'J', "111111110"}, {'K', "11111111110"}, {'L', "111010"},
-        {'M', "1100"}, {'N', "10110"}, {'O', "011"},
-        {'P', "111011"}, {'Q', "1111100"}, {'R', "1001"},
-        {'S', "1000"}, {'T', "11011"}, {'U', "11010"},
-        {'V', "1111000"}, {'W', "111111111110"}, {'X', "1111111110"},
-        {'Y', "111111111111"}, {'Z', "11111110"},
+        {'a', "010"}, {'b', "1111100"}, {'c', "11011"},
+        {'d', "10111"}, {'e', "001"}, {'f', "1111101"},
+        {'g', "111100"}, {'h', "1111010"}, {'i', "1010"},
+        {'j', "11111110"}, {'k', "1111111110"}, {'l', "11100"},
+        {'m', "11000"}, {'n', "10110"}, {'o', "011"},
+        {'p', "111010"}, {'q', "1111011"}, {'r', "1001"},
+        {'s', "1000"}, {'t', "11010"}, {'u', "11001"},
+        {'v', "111011"}, {'w', "11111111110"}, {'x', "111111110"},
+        {'y', "11111111111"}, {'z', "1111110"}, {' ', "000"},
+        {'A', "010"}, {'B', "1111100"}, {'C', "11011"},
+        {'D', "10111"}, {'E', "001"}, {'F', "1111101"},
+        {'G', "111100"}, {'H', "1111010"}, {'I', "1010"},
+        {'J', "11111110"}, {'K', "1111111110"}, {'L', "11100"},
+        {'M', "11000"}, {'N', "10110"}, {'O', "011"},
+        {'P', "111010"}, {'Q', "1111011"}, {'R', "1001"},
+        {'S', "1000"}, {'T', "11010"}, {'U', "11001"},
+        {'V', "111011"}, {'W', "11111111110"}, {'X', "111111110"},
+        {'Y', "11111111111"}, {'Z', "1111110"},
     };
 
     ifstream arquivoEntrada("../input.txt");
@@ -70,7 +74,11 @@ int main() {
     arquivoEntrada.close();
 
     int totalBits, numCaracteresCodificados;
-    vector<uint8_t> textoCodificado = codificarTexto(conteudo, mapaCodigo, totalBits, numCaracteresCodificados);
+    string bitsCodificados;
+    vector<uint8_t> textoCodificado = codificarTexto(conteudo, mapaCodigo, totalBits, numCaracteresCodificados, bitsCodificados);
+
+    // Exibir a sequência de bits codificada
+    cout << "Texto codificado em bits:\n" << bitsCodificados << endl;
 
     ofstream arquivoSaida("../texto_codificado.bin", ios::binary);
     arquivoSaida.write(reinterpret_cast<const char*>(textoCodificado.data()), textoCodificado.size());
